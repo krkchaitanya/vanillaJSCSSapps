2d32fd1aa77470c3ec5ba1b0593ee5c318614d85
krkchaitanya <krkchaitanya42@gmail.com>
2021-03-30 20:45:34 -0600

    sets algo changes continued

--- algorithmsandDS/stack.js
+++ algorithmsandDS/stack.js
@@ -32,6 +32,12 @@ console.log(" Palindrome check --racecar--> ", isPalindrome("racecar"));
 console.log(" Palindrome check --bucket--> ", isPalindrome("bucket"));
 
 
+/*
+--------------------
+------ Stack -------
+--------------------
+*/
+
 var Stack = function() {
 
     this.count = 0;
@@ -97,6 +103,13 @@ var mydataset = function() {
         return collection.indexOf(dataele) !== -1;
     }
 
+
+    // return all the elements present in the set
+    this.values = function() {
+        return collection;
+    }
+
+
     // add element to the set
     this.add = function(dataele) {
 
@@ -108,6 +121,7 @@ var mydataset = function() {
             return true;
         }
     }
+
 
     //remove element from the set
     this.remove = function(dataele) {
@@ -123,12 +137,91 @@ var mydataset = function() {
         }
     }
 
+
     // size of the set
     this.size = function() {
         return collection.length;
     }
 
+
+    // Union
+    this.union = function(inputset) {
+
+        var unionset = new mydataset();
+        var firstset = this.values();
+        var secondset = inputset.values();
+
+        firstset.forEach(function(e) { unionset.add(e) });
+        secondset.forEach(function(e) { unionset.add(e) });
+
+        return unionset;
+    }
+
+
+    // intersection
+    this.intersection = function(otherset) {
+
+        var intersectionSet = new mydataset();
+        var currentsetVals = this.values();
+
+        currentsetVals.forEach(function(elem) {
+            if(otherset.has(elem)) {
+                intersectionSet.add(elem);
+            }
+        });
+
+        return intersectionSet.values();
+    }
+
+
+    // difference
+    this.difference = function(otherset) {
+
+        var differenceset = new Set();
+        var currentset = this.values();
+
+        currentset.forEach(function(elem) {
+            if(!otherset.has(elem)) {
+                differenceset.add(elem);
+            }
+        });
+
+        otherset.values().forEach(function(elem) {
+            if(!differenceset.has(elem) && !currentset.includes(elem)) {
+                differenceset.add(elem);
+            }
+        })
+
+        return differenceset.values();
+    }
+
+
+    // isSubset
+    this.issubset = function(otherset) {
+
+        var newResultSet = new Set();
+        var currentset = this.values();
+
+        currentset.forEach(function(elem) {
+            if(otherset.has(elem)) {
+                newResultSet.add(elem);
+            }
+        });
+
+        return newResultSet.size === otherset.size ? true : false;
+
+    }
+
 }
+
+
+
+
+
+
+/*
+------- Validations -------
+*/
 
 const dataset = new mydataset();
 
@@ -138,16 +231,35 @@ console.log("--mydataset--", dataset.has("itemthree"));
 console.log("--mydataset--", dataset.add("itemthree"));
 console.log("--mydataset--", dataset.size());
 console.log("--mydataset--", dataset.remove("itemthree"));
+console.log("--mydataset--", dataset.add("itemtwo"));
 console.log("--mydataset--", dataset.size());
 
+var seconddataset = new mydataset();
+
+seconddataset.add("secondone");
+
+var unionsetresults = dataset.union(seconddataset);
+console.log(" -- unionsetresults -- ", unionsetresults.values());
 
 
+var set1 = new mydataset();
+set1.add("one");
+set1.add("two");
+set1.add("three");
+
+var set2 = new mydataset();
+set2.add("one");
+set2.add("three");
+
+var intersectionResult = set1.intersection(set2);
+console.log(" -- intersectionResult -- ", intersectionResult);
+
+var differenceResult = set1.difference(set2);
+console.log(" --differenceResult-- ", differenceResult);
 
 
-
-
-
-
-
-
-
+var subst = new Set();
+subst.add("one");
+subst.add("two");
+var isitasubset = set1.issubset(subst);
+console.log(" --isitasubset-- ", isitasubset);
